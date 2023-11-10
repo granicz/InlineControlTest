f1ccd8b2c1fa958d0eeee9aff79e97e246c4971c
Adam Granicz <adam.granicz@gmail.com>
2023-11-10 19:24:56 +0100

    Use a master template for the Home page.

--- Site.fs
+++ Site.fs
@@ -4,6 +4,7 @@ open WebSharper
 open WebSharper.Sitelets
 open WebSharper.UI.Server
 open WebSharper.UI.Html
+open WebSharper.UI.Templating
 
 type EndPoint =
     | [<EndPoint "/">] Home
@@ -15,6 +16,8 @@ type MyControl() =
     override this.Body =
         div [] [text "This is a web.control instance"]
 
+type Templates = Template<"wwwroot\\template.html", ClientLoad.Inline, ServerLoad.WhenChanged>
+
 module Site =
     open type WebSharper.UI.ClientServer
     open WebSharper.JavaScript
@@ -25,15 +28,18 @@ module Site =
             match endpoint with
             | EndPoint.Home ->
                 let a = "hello"
-                Content.Page([
-                    client (div [] [text a])
-                    br [
-                        // ERROR: This won't print anything
-                        on.afterRender (fun e -> Console.Log $"br OAR={e}")
-                    ] []
-                    div [
-                        // ERROR: uncommenting causes a runtime error
-                        //on.click (fun e args -> Console.Log $"div onclick={e}")
-                    ] [Doc.WebControl <| MyControl()]
-                ])
+                Templates()
+                    .Body([
+                        client (div [] [text a])
+                        br [
+                            // ERROR: This won't print anything
+                            on.afterRender (fun e -> Console.Log $"br OAR={e}")
+                        ] []
+                        div [
+                            // ERROR: uncommenting causes a runtime error
+                            on.click (fun e args -> Console.Log $"div onclick={e}")
+                        ] [Doc.WebControl <| MyControl()]
+                    ])
+                    .Doc()
+                |> Content.Page
         )
