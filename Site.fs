41b514d8d55d28388ae183707443f711b2f8c967
Adam Granicz <adam.granicz@gmail.com>
2023-11-10 22:11:09 +0100

    Added a template-driven control and numbered inner "tests".

--- Site.fs
+++ Site.fs
@@ -9,12 +9,12 @@ open WebSharper.UI.Templating
 type EndPoint =
     | [<EndPoint "/">] Home
 
-type MyControl() =
+type MyControl(a: string) =
     inherit Web.Control()
 
     [<JavaScript>]
     override this.Body =
-        div [] [text "This is a web.control instance"]
+        div [] [text $"{a} This is a web.control instance"]
 
 type Templates = Template<"wwwroot\\template.html", ClientLoad.Inline, ServerLoad.WhenChanged>
 
@@ -28,17 +28,29 @@ module Site =
             match endpoint with
             | EndPoint.Home ->
                 let a = "hello"
+                // The master document is coming from a template, with ...
                 Templates()
+                    .Title("Hello")
                     .Body([
-                        client (div [] [text a])
-                        br [
-                            // ERROR: This won't print anything
-                            on.afterRender (fun e -> Console.Log $"br OAR={e}")
-                        ] []
+                        // 1. Embedded client-side code
+                        client (div [] [text $"1. non-hydrated {a}"])
+                        // 2. Hydrated client-side code
+                        hydrate (div [] [text $"2. hydrated {a}"])
+                        // 3. An OAR handler
                         div [
-                            // ERROR: uncommenting causes a runtime error
+                            on.afterRender (fun e -> Console.Log $"br OAR={e}")
+                        ] [text "3."]
+                        // 4. A click event handler
+                        div [
                             on.click (fun e args -> Console.Log $"div onclick={e}")
-                        ] [Doc.WebControl <| MyControl()]
+                        ] [text "4. Click me"]
+                        // 5. A client-side control
+                        Doc.WebControl <| MyControl("5.")
+                        // 6. A template-driven "component" with event binding
+                        Templates.MyComponent()
+                            .Title("6. MyButton")
+                            .OnClick(fun e -> Console.Log $"MyButton onclick => Textbox has \"{e.Vars.Textbox.Value}\"")
+                            .Doc()
                     ])
                     .Doc()
                 |> Content.Page
